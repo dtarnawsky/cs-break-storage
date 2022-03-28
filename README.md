ef0c8fab336ae20dfca8a29a380908375ce67234
Damian Tarnawsky <damian@ionic.io>
2022-03-28 11:42:00 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sample Project
+This project uses Secure Storage and attempts to continuously create transactions to test what happens when the app is backgrounded.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import { SQLite, SQLiteObject, DbTransaction } from '@ionic-enterprise/secure-storage/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +7,60 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  private database: SQLiteObject;
+
+  constructor(private sqlite: SQLite) {
+    this.init();
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  async init() {
+    try {
+      const db = await this.sqlite.create({
+        name: 'mydb',
+        location: 'default',
+        // Key/Password used to encrypt the database
+        // Strongly recommended to use Identity Vault to manage this
+        key: 'password'
+      });
+
+      this.database = db;
+
+      // Create our initial schema
+      await db.executeSql('CREATE TABLE IF NOT EXISTS software(name, company, type, version)', [])
+    } catch (e) {
+      console.error('Unable to initialize database', e);
+    }
+  }
+
+  insert() {
+    this.database.transaction((tx: DbTransaction) => {
+      tx.executeSql('INSERT INTO software (name, company, type, version) VALUES (?,?,?,?)',
+        ['secure-storage', 'ionic', 'native', '2.0'], (tx, result) => {
+//          console.log('insertId: ' + result.insertId);  // New Id number
+//          console.log('rowsAffected: ' + result.rowsAffected);  // 1
+        });
+    });
+  }
+
+  select() {
+    this.database.transaction(tx => {
+      tx.executeSql('SELECT * from software', [], (tx, result) => {
+        // Rows is an array of results. Use zero-based indexing to access
+        // each element in the result set: item(0), item(1), etc.
+        // for (let i = 0; i < result.rows.length; i++) {
+        //   console.log(result.rows.item(i));
+        //   console.log(result.rows.item(i).company);
+        // }
+      });
+    });
+  }
+
+  selectTest() {
+    setInterval(() => {
+      this.select();
+      this.insert();
+    }, 1);
   }
 
 }
